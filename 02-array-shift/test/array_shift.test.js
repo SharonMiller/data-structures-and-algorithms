30ee936d07246366a7399c080ec65b88113a94e1
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-08 11:48:04 -0700

    add tests to array_shift, test pass

--- 02-array-shift/test/array_shift.test.js
+++ 02-array-shift/test/array_shift.test.js
@@ -2,9 +2,25 @@
 
 const array_shift = require('../lib/array_shift');
 
-test('should insert key into middle of an even array', () => {
-  const test = [1, 3, 6, 7, 22, 33]
-  let actual = insertShiftArray(test, 'middle');
-  let expectedValue = [1, 3, 6, 'middle', 7, 22, 33];
-  expect(actual).toEqual(expectedValue); 
+describe('insertShiftArray tests', () => {
+  test('should insert key into middle of an even array', () => {
+    const test = [1, 3, 6, 7, 22, 33]
+    let actual = array_shift.insertShiftArray(test, 'middle');
+    let expectedValue = [1, 3, 6, 'middle', 7, 22, 33];
+    expect(actual).toEqual(expectedValue);
+  });
+
+  test('should insert key into middle rounded up of an odd array', () => {
+    const test = [1, 3, 6, 7, 22]
+    let actual = array_shift.insertShiftArray(test, 'middle');
+    let expectedValue = [1, 3, 6, 'middle', 7, 22];
+    expect(actual).toEqual(expectedValue);
+  });
+
+  test('should add item to array', () => {
+    let actual = array_shift.insertShiftArray([], 5);
+    let expectedValue = [5];
+    expect(actual).toEqual(expectedValue);
+  });
+
 });
