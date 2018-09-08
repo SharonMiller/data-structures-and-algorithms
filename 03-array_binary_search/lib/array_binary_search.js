aedbdfdcd6e82ac12f871a28a55700d939ba152a
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-08 14:41:34 -0700

    code is passing all three tests

---
+++ 03-array_binary_search/lib/array_binary_search.js
@@ -0,0 +1,24 @@
+'use strict';
+/*arr - ordered list to search from
+target - number to search for
+return index of element or -1 if element not found */
+
+module.exports = exports = {};
+exports.binarySearch = (arr, target) => {
+  let end = arr.length - 1;
+  let beg = 0;
+
+  while (beg <= end) {
+    let mid = Math.floor((beg + end) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] > target) {
+      end = mid - 1;
+    } else {
+      beg = mid + 1;
+    }
+  }
+  return -1;
+}
+
+// exports.binarySearch(test, 6);
+// console.log(binarySearch())
